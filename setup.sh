dbfcc4d4629b241f7e0f98a4f6ff437edaee54bf
Stoian Alin <stoian.bogdan03@gmail.com>
2024-01-10 17:06:43 +0200

    added kafka and test logic for it

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Setup pyspark environment
 sudo chmod 777 ./notebooks # This is needed for the mount to work -> different gid in the container
-sudo docker run --group-add=100 -p 8888:8888 -v $(pwd)/notebooks/:/home/jovyan/notebooks/ -d jupyter/pyspark-notebook
+sudo docker run --group-add=100 -p 8888:8888 -v $(pwd)/notebooks/:/home/jovyan/notebooks/ --net host -d jupyter/pyspark-notebook
 
 # Download the dataset if it isn't already downloaded - do not commit it, its huge!
 cd ./notebooks
@@ -10,3 +10,7 @@ if ! [ -f ./dataset.csv ]; then
 	curl "https://data.cityofnewyork.us/api/views/8h9b-rp9u/rows.csv?date=20240105&accessType=DOWNLOAD" -o dataset.csv
 fi
 
+# Build and run kafka instance
+cd ../kafka
+sudo docker build . -t kafka
+sudo docker run -d -p 9092:9092 --net host kafka
