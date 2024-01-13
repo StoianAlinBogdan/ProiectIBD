9afd0bd92407d398852abfe9e84f6bc96d5f90c4
CusCatalinAlexandru <catalin.cus.alexandru@gmail.com>
2024-01-13 17:10:33 +0200

    Updatebackupsh

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Creare backup la baza de date
-sudo docker exec -it MYSQL mysqldump -uroot -p123 IBD > BACKUPIBD.sql
+sudo docker exec -i MYSQL mysqldump -u root -p123 IBD --skip-comments > BACKUPIBD.sql
