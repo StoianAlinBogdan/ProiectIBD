54eace7adcaf456b0d202e56367fff5368ce7441
CusCatalinAlexandru <catalin.cus.alexandru@gmail.com>
2024-01-13 18:00:01 +0200

    stergerefisieredinmaindirec

--- backupreplica.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Replicate the database backup on another instance
-
-# Create a database replica
-sudo docker exec -it MYSQL_REPLICA mysql -u root -p123 -e "CREATE DATABASE IF NOT EXISTS IBDREPLICA"
-
-# Import the backup
-sudo docker exec -i MYSQL_REPLICA mysql -u root -p123 IBDREPLICA < BACKUPIBD.sql
